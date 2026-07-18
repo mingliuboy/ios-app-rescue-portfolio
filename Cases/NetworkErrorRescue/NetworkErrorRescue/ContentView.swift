6d50486e422c426e75badd044b982572e7a9762c
Carl <ballming@163.com>
2026-07-18 11:58:13 +0800

    Add NetworkErrorRescue case scaffold

---
+++ Cases/NetworkErrorRescue/NetworkErrorRescue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkErrorRescue
+//
+//  Created by Carl on 2026/7/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cases/NetworkErrorRescue/NetworkErrorRescue/NetworkErrorRescueApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkErrorRescueApp.swift
+//  NetworkErrorRescue
+//
+//  Created by Carl on 2026/7/18.
+//
+
+import SwiftUI
+
+@main
+struct NetworkErrorRescueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
